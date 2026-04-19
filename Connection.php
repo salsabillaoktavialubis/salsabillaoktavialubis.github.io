5b1d5c0cd017149587a1b5bb006a2069f29858b8
salsabillaoktavialubis <salsabillaokt13@gmail.com>
2026-04-19 17:39:58 +0700

    Update Connection.php

--- Connection.php
+++ Connection.php
@@ -1,1 +1,7 @@
+<?php
+$conn = mysqli_connect("sql209.infinityfree.com", "if0_41069745", "Salsabillaokt13", "if0_41069745_Perpustakaan_SD_Taman_Ceria");
 
+if (!$conn) {
+    die("Koneksi gagal");
+}
+?>
